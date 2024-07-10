1394e0f2c285eb4e3c23592a52736b730ab71c1c
TomSowyer <tomsowyertm@gmail.com>
2024-07-10 16:12:34 +0300

    11

--- src/leson.ts
+++
@@ -1,8 +0,0 @@
-export class Methods {
-    static sortNumberArray(array: number[], type = 'asc' || 'desc') {
-        if (type == 'asc') array.sort((a: number, b: number) => {
-            return a - b;
-        });
-        return array.sort();
-    }
-}
